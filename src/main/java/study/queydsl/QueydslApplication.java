1820cc479df544bddb65f8e1fb83231a6bbf430b
2rohyun <dlehgus5656@naver.com>
2021-07-02 13:47:00 +0900

    first commit

---
+++ src/main/java/study/queydsl/QueydslApplication.java
@@ -0,0 +1,13 @@
+package study.queydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueydslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/queydsl/QueydslApplicationTests.java
@@ -0,0 +1,13 @@
+package study.queydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
